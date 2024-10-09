3696fdf0036c95158cb58c51fbd2c66d869f1326
Kanita <kanitalim88@gmail.com>
2024-10-09 10:38:58 +0200

    Add files via upload

---
+++ jeunesParents/jeunesParents/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  jeunesParents
+//
+//  Created by Apprenant 172 on 09/10/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ jeunesParents/jeunesParents/jeunesParentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  jeunesParentsApp.swift
+//  jeunesParents
+//
+//  Created by Apprenant 172 on 09/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct jeunesParentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

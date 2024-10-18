9613ee101c93f6da0c10554474fc453a88ce165b
Dembod <diaouned@yahoo.fr>
2024-10-18 15:06:47 +0200

    création branche + création ficher model

---
+++ jeunesParents/jeunesParents/Model/Register.swift
@@ -0,0 +1,13 @@
+//
+//  Register.swift
+//  jeunesParents
+//
+//  Created by Apprenant 142 on 18/10/2024.
+//
+
+import Foundation
+
+class Register: Codable {
+    let email: String
+    let password: String
+}

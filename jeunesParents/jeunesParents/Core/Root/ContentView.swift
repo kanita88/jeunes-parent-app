67d9a7e07ee8c0fc9aae9adb3fb3b7ecc1930120
Kanita <kanitalim88@gmail.com>
2024-10-23 11:08:12 +0200

    Change content view

--- jeunesParents/jeunesParents/Core/Root/ContentView.swift
+++ jeunesParents/jeunesParents/Core/Root/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        //AuthentificationView(authViewModel: AuthentificationViewModel())
-        HomeView()
+        AuthentificationView(authViewModel: AuthentificationViewModel())
     }
 }
 
